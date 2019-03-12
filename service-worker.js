58392ad63574efd931a049ca0c2d3fd67672862f
Andrew Johnson <abjohnson395@gmail.com>
2019-03-12 09:27:52 -0400

    Added offline caching

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,25 @@
 var cacheName = 'weatherPWA';
-var filesToCache = [];
+var dataCacheName = 'weatherData-v1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/scripts/fakeforecasts.js',
+    '/styles/inline.css',
+    '/images/clear.png',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+];
 
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
@@ -11,6 +31,42 @@ self.addEventListener('install', function(e) {
     );
 });
 
+// When Service Worker is activated, update the cache by changing name
 self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+
+    // Speeds up service worker activation to prevent getting old data from cache
+    return self.clients.claim();
+});
+
+// Fetching from the cache
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response) {
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
 });
